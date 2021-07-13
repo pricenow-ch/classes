pricenow-ch/classes

commit 6ff7cbd99037d54f17ec4570c526cdad1546bd82

remove basepeapi

basket/AdminBasket.js
@@ -39,11 +39,8 @@ export default class AdminBasket extends Basket {
         : discountAmount * 100
 
     try {
-      let response = await axios.post(
-        store.getters.getCurrentDestinationInstance().getBasePeApi() +
-          'admin/' +
-          this.getUuid() +
-          '/group_discounts',
+      let response = await peInstance.post(
+        `/admin/${this.getUuid()}/group_discounts`,
         {
           kind: discountType,
           amount: amount,
@@ -67,14 +64,12 @@ export default class AdminBasket extends Basket {
     EventBus.$emit('spinnerShow')
 
     try {
-      let response = await axios.delete(
-        store.getters.getCurrentDestinationInstance().getBasePeApi() +
-          'admin/' +
-          this.getUuid() +
-          '/' +
-          store.getters.getCurrentDestinationInstance().getSlug() +
-          '/group_discounts/' +
-          groupDiscountId
+      let response = await peInstance.delete(
+        `/admin/${this.getUuid()}/${
+          process.env.VUE_APP_DESITNATION
+        }/group_discounts/${store.getters
+          .getCurrentDestinationInstance()
+          .getSlug()}`
       )
 
       await this.parseApiData(response.data)

axess/AxessManagementService.js
@@ -1,5 +1,6 @@
 /** api interface class to talk with the api **/
 import Season from '@/classes/season/Season'
+import { peInstance } from '../utils/axiosInstance'
 
 export default class AxessManagementService {
   /**
@@ -16,19 +17,15 @@ export default class AxessManagementService {
       return false
     }
     try {
-      let response = await axios.get(
-        store.getters.getCurrentDestinationInstance().getBasePeApi() +
-          'admin/imports/axess/newSeasons',
-        {
-          params: {
-            destinationNames: destinationSlug,
-          },
-        }
-      )
+      const { data } = await peInstance.get('/admin/imports/axess/newSeasons', {
+        params: {
+          destinationNames: destinationSlug,
+        },
+      })
       // create model instances
       // iterate seasons
       let peSeasons = []
-      const rawSeasons = response.data
+      const rawSeasons = data
       for (const rawSeason of rawSeasons) {
         peSeasons.push(new Season(rawSeason))
       }
@@ -49,11 +46,11 @@ export default class AxessManagementService {
       return i18n.t('axessManagementService.noDestinationProvided')
     }
     try {
-      await axios.patch(
-        store.getters.getCurrentDestinationInstance().getBasePeApi() +
-          'admin/imports/axess/newSeasons?destinationNames=' +
-          destinationSlug
-      )
+      await peInstance.patch(`'/admin/imports/axess/newSeasons`, undefined, {
+        params: {
+          destinationNames: destinationSlug,
+        },
+      })
       return true
     } catch (error) {
       return error.response.data.error

destinations/UserDestination.js
@@ -1,6 +1,7 @@
 import Destination from './Destination'
 import store from '../../store/store'
 import DateHelper from '../DateHelper'
+import { peInstance } from '../utils/axiosInstance'
 /**
  * Child class of destination used mostly in AppUserInstance. Maybe also in the filter of the pricing engine.
  */
@@ -42,13 +43,10 @@ export default class UserDestination extends Destination {
     this.fromDateInstance = from
     this.toDateInstance = to
 
-    let baseUrl = store.getters
-      .getCurrentDestinationInstance()
-      .getBasePeApi() /* global EventBus axios */
     EventBus.$emit('spinnerShow')
 
     try {
-      let response = await axios.get(baseUrl + 'admin/expected_demands', {
+      const response = await peInstance.get('/admin/expected_demands', {
         params: {
           from: DateHelper.shiftLocalToUtcIsoString(this.fromDateInstance),
           to: DateHelper.shiftLocalToUtcIsoString(this.toDateInstance),

files/FileUploadService.js
@@ -1,4 +1,4 @@
-import { shopInstance } from '../utils/axiosInstance'
+import { peInstance, shopInstance } from '../utils/axiosInstance'
 import FileModel from './FileModel'
 
 export default class FileUploadService {
@@ -53,9 +53,8 @@ export default class FileUploadService {
       /* global EventBus axios i18n */
       EventBus.$emit('spinnerShow', i18n.t('fileUploadService.processingExcel'))
 
-      let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
       try {
-        await axios.post(baseUrl + 'admin/imports', formData, {
+        await peInstance.post('/admin/imports', formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },

products/CashDeskPriceService.js
@@ -1,6 +1,7 @@
 import moment from 'moment'
 import store from '@/store/store'
 import CashDeskPrice from '@/classes/products/CashDeskPrice'
+import { peInstance } from '../utils/axiosInstance'
 
 export default class CashDeskPriceService {
   /**
@@ -12,12 +13,11 @@ export default class CashDeskPriceService {
    * @returns {Promise<[]>}
    */
   async fetchBetween(from = new Date(), to = new Date(), prodDefIds = []) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     let cashDeskPrices = []
     try {
-      let response = await axios.get(baseUrl + 'admin/cash_desk_price', {
+      let response = await peInstance.get('/admin/cash_desk_price', {
         params: {
           from: moment(from).format('YYYY-MM-DD'),
           to: moment(to).format('YYYY-MM-DD'),
@@ -54,11 +54,10 @@ export default class CashDeskPriceService {
    * @returns {Promise<void>}
    */
   async createCustomCashDeskPrice(cashDeskPriceObj, newPrice) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     try {
-      await axios.post(baseUrl + 'admin/daily_base_rates', {
+      await peInstance.post('/admin/daily_base_rates', {
         date: cashDeskPriceObj.date,
         price: newPrice,
         productDefinitionId: cashDeskPriceObj.productDefinitionId,
@@ -78,12 +77,11 @@ export default class CashDeskPriceService {
    * @returns {Promise<void>}
    */
   async updateCustomCashDeskPrice(cashDeskPriceObj, newPrice) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     try {
       const { id } = cashDeskPriceObj
-      await axios.put(baseUrl + 'admin/cash_desk_price/' + id, {
+      await peInstance.put('/admin/cash_desk_price/' + id, {
         price: newPrice,
       })
     } catch (e) {
@@ -101,12 +99,11 @@ export default class CashDeskPriceService {
    * @returns {Promise<void>}
    */
   async reset(cashDeskPriceObj) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     try {
       const id = cashDeskPriceObj.id
-      await axios.delete(baseUrl + 'admin/cash_desk_price/' + id)
+      await peInstance.delete(`/admin/cash_desk_price/${id}`)
     } catch (e) {
       console.error('err', e)
       EventBus.$emit('notify', e.response)

products/CurrentPostedPriceService.js
@@ -1,6 +1,7 @@
 import store from '../../store/store'
 import moment from 'moment'
 import CurrentPostedPrice from '@/classes/products/CurrentPostedPrice'
+import { peInstance } from '../utils/axiosInstance'
 
 export default class CurrentPostedPriceService {
   /**
@@ -16,13 +17,11 @@ export default class CurrentPostedPriceService {
     to = new Date(),
     productDefinitionIds = []
   ) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
-
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     let prices = []
     try {
-      let response = await axios.get(baseUrl + 'admin/prices/current', {
+      const { data, status } = await peInstance.get('/admin/prices/current', {
         params: {
           from: moment(from).format('YYYY-MM-DD'),
           to: moment(to).format('YYYY-MM-DD'),
@@ -30,12 +29,10 @@ export default class CurrentPostedPriceService {
         },
       })
 
-      if (response.status === 200) {
-        response.data.prices.forEach((price) => {
-          prices.push(
-            new CurrentPostedPrice(price, response.data.lastCalculationDate)
-          )
-        })
+      if (status === 200) {
+        prices = data.prices.map(
+          (price) => new CurrentPostedPrice(price, data.lastCalculationDate)
+        )
       } else {
         EventBus.$emit('notify')
       }

products/DailyBaseRateService.js
@@ -1,6 +1,7 @@
 import moment from 'moment'
 import store from '@/store/store'
 import DailyBaseRate from '@/classes/products/DailyBaseRate'
+import { peInstance } from '../utils/axiosInstance'
 
 /**
  * Daily Base Rate Service
@@ -16,12 +17,11 @@ export default class DailyBaseRateService {
    * @returns {Promise<[]>}
    */
   async fetchBetween(from = new Date(), to = new Date(), productIds = []) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     let baseRates = []
     try {
-      let response = await axios.get(baseUrl + 'admin/daily_base_rates', {
+      const { data, status } = await peInstance.get('/admin/daily_base_rates', {
         params: {
           from: moment(from).format('YYYY-MM-DD'),
           to: moment(to).format('YYYY-MM-DD'),
@@ -29,10 +29,8 @@ export default class DailyBaseRateService {
         },
       })
 
-      if (response.status === 200) {
-        response.data.forEach((baseRate) => {
-          baseRates.push(new DailyBaseRate(baseRate))
-        })
+      if (status === 200) {
+        baseRates = data.map((baseRate) => new DailyBaseRate(baseRate))
       } else {
         EventBus.$emit('notify')
       }
@@ -67,11 +65,10 @@ export default class DailyBaseRateService {
    * @returns {Promise<void>}
    */
   async createCustomDailyBaseRate(dailyBaseRateObj, newDailyBaseRate) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     try {
-      await axios.post(baseUrl + 'admin/daily_base_rates', {
+      await peInstance.post('/admin/daily_base_rates', {
         date: dailyBaseRateObj.date,
         dailyBaseRate: newDailyBaseRate,
         productId: dailyBaseRateObj.productId,
@@ -91,12 +88,11 @@ export default class DailyBaseRateService {
    * @returns {Promise<void>}
    */
   async updateCustomDailyBaseRate(dailyBaseRateObj, newDailyBaseRate) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     try {
       const id = dailyBaseRateObj.customBaseRateId
-      await axios.put(baseUrl + 'admin/daily_base_rates/' + id, {
+      await peInstance.put(`/admin/daily_base_rates/${id}`, {
         dailyBaseRate: newDailyBaseRate,
       })
     } catch (e) {
@@ -114,12 +110,11 @@ export default class DailyBaseRateService {
    * @returns {Promise<void>}
    */
   async reset(dailyBaseRateObj) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     try {
       const id = dailyBaseRateObj.customBaseRateId
-      await axios.delete(baseUrl + 'admin/daily_base_rates/' + id)
+      await peInstance.delete(`/admin/daily_base_rates/${id}`)
     } catch (e) {
       console.error('err', e)
       EventBus.$emit('notify', e.response)

products/MaximumEarlyBookingDiscountService.js
@@ -1,6 +1,7 @@
 import store from '../../store/store'
 import moment from 'moment'
 import MaximumEarlyBookingDiscount from '@/classes/products/MaximumEarlyBookingDiscount'
+import { peInstance } from '../utils/axiosInstance'
 
 export default class MaximumEarlyBookingDiscountService {
   /**
@@ -17,7 +18,6 @@ export default class MaximumEarlyBookingDiscountService {
     to = new Date(),
     productDefinitionIds = []
   ) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     let maxEarlyBookingDiscounts = []
@@ -31,19 +31,17 @@ export default class MaximumEarlyBookingDiscountService {
         params['priceModel'] = 'winter_3'
       }
 
-      let response = await axios.get(
-        baseUrl + 'admin/max_early_booking_discount',
+      const { status, data } = await peInstance.get(
+        '/admin/max_early_booking_discount',
         {
           params,
         }
       )
 
-      if (response.status === 200) {
-        response.data.forEach((earlyBooking) => {
-          maxEarlyBookingDiscounts.push(
-            new MaximumEarlyBookingDiscount(earlyBooking)
-          )
-        })
+      if (status === 200) {
+        maxEarlyBookingsDiscounts = data.map(
+          (earlyBooking) => new MaximumEarlyBookingDiscount(earlyBooking)
+        )
       } else {
         EventBus.$emit('notify')
       }
@@ -84,11 +82,10 @@ export default class MaximumEarlyBookingDiscountService {
    * @returns {Promise<void>}
    */
   async createMaxEarlyBookingDiscount(maxEarlyBookingObjs, newDiscountFactor) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     try {
-      await axios.post(baseUrl + 'admin/max_early_booking_discount', {
+      await peInstance.post('/admin/max_early_booking_discount', {
         date: maxEarlyBookingObjs.date,
         productdefinitionid: maxEarlyBookingObjs.productDefinitionId,
         factor: newDiscountFactor,
@@ -108,12 +105,11 @@ export default class MaximumEarlyBookingDiscountService {
    * @returns {Promise<void>}
    */
   async updateMaxEarlyBookingDiscount(maxEarlyBookingObj, newDiscountFactor) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     try {
       const id = maxEarlyBookingObj.customMaxEarlyBookingDiscountId
-      await axios.put(baseUrl + 'admin/max_early_booking_discount/' + id, {
+      await peInstance.put(`/admin/max_early_booking_discount/${id}`, {
         factor: newDiscountFactor,
       })
     } catch (e) {
@@ -131,12 +127,11 @@ export default class MaximumEarlyBookingDiscountService {
    * @returns {Promise<void>}
    */
   async reset(maxEarlyBookingObj) {
-    let baseUrl = store.getters.getCurrentDestinationInstance().getBasePeApi()
     /* global EventBus axios */
     EventBus.$emit('spinnerShow')
     try {
       const id = maxEarlyBookingObj.customMaxEarlyBookingDiscountId
-      await axios.delete(baseUrl + 'admin/max_early_booking_discount/' + id)
+      await peInstance.delete(`/admin/max_early_booking_discount/${id}`)
     } catch (e) {
       console.error('err', e)
       EventBus.$emit('notify', e.response)

commit a2b3d0224b736bc5be1a0f87742681b3a75ade64

fix group discount broken by 6ff7cbd99037d54f17ec4570c526cdad1546bd82

basket/AdminBasket.js
@@ -65,11 +65,9 @@ export default class AdminBasket extends Basket {
 
     try {
       let response = await peInstance(false).delete(
-        `/admin/${this.getUuid()}/${
-          process.env.VUE_APP_DESITNATION
-        }/group_discounts/${store.getters
+        `/admin/${this.getUuid()}/${store.getters
           .getCurrentDestinationInstance()
-          .getSlug()}`
+          .getSlug()}/group_discounts/${groupDiscountId}`
       )
 
       await this.parseApiData(response.data)